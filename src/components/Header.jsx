9725192ecfccd07d96ad3001ba8aaedbcbb78b34
Front-End-Dev-web <wa5236759@gmail.com>
2025-03-13 12:12:10 +0500

    @@

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap-icons/font/bootstrap-icons.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import '../index.css'
 import '../App.css'
